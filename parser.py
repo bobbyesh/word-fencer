6eef7a3029201276bf183027a13455f7a083cf79
Bobby Eshleman <eshleman@pdx.edu>
2016-06-23 20:41:22 +0000

    Implemented trie add_string and find_match

--- parser.py
+++ parser.py
@@ -3,7 +3,38 @@
 '''
 This parser will parser any string of text of any language which has no delimiters.
 These are languages such as Chinese, Hindi, Thai, etc...
+
 '''
+
+from itertools import zip_longest
+
+
+class Trie:
+
+    def __init__(self):
+        self.root = dict()
+
+    def add_string(self, string):
+        current_node = self.root
+        for pair in zip_longest(string, string[1:]):
+            if (pair[0] not in current_node and
+                pair[1] is not None):
+                current_node[pair[0]] = { pair[1] : dict() }
+            elif (pair[0] not in current_node and
+                pair[1] is None):
+                current_node[pair[0]] = pair[1]
+            current_node = current_node[pair[0]]
+
+    def find_match(self, string):
+        current_node = self.root
+        try:
+            for c in string:
+                current_node = current_node[c]
+        except KeyError:
+            return False
+        return True
+
+
 
 class Parser:
 
@@ -13,13 +44,13 @@ class Parser:
         Words must be set before building the trie.
         '''
         self.words = word_file
-        self.dictionary = self.build_trie()
+        self.trie = self.build_trie()
         pass
 
     def parse(self, string):
         result = ''
         tokens = list()
-        current = self.dictionary
+        current = self.trie
         prev = 0
         for ix,s in enumerate(string):
             if current[s]:
@@ -27,7 +58,7 @@ class Parser:
             elif current[s] == None:
                 tokens.append(string[prev:ix+1])
                 prev = ix+1
-                current = self.dictionary
+                current = self.trie
         return tokens
 
     def set_wordref(self, file):
@@ -37,15 +68,13 @@ class Parser:
         self.words = file
 
     def build_trie(self):
-        test_dict = "ABC"
-        k = dict()
-        for ix,char in enumerate(reversed(test_dict)):
-            if ix == 0:
-                prev = { char : None }
-            else:
-                current = { char : prev }
-                prev = current
-        dictionary = prev
-        dictionary["D"] = None
-        dictionary["E"] = None
-        return dictionary
+        trie_root = dict()
+        for word in self.words:
+            for ix,right_char in enumerate(reversed(word)):
+                if ix == 0:
+                    left_char = { right_char : None }
+                else:
+                    current = { right_char : left_char }
+                    left_char = current
+            trie_root = left_char
+        self.trie = trie_root
--- tests.py
+++ tests.py
@@ -2,6 +2,58 @@ import unittest
 import parser
 
 
+class TestTrie(unittest.TestCase):
+
+    def test_one_entry(self):
+        trie = parser.Trie()
+        trie.add_string('ABC')
+        self.assertTrue(trie.find_match('ABC'))
+        self.assertTrue(trie.root == {'A' : {'B': {'C': None}}})
+
+    def test_two_entry(self):
+        '''
+        Trie matches 'ABC' after adding 'ABC' and 'Axx'.
+        '''
+        trie = parser.Trie()
+        trie.add_string('ABC')
+        trie.add_string('Axx')
+        self.assertTrue(trie.find_match('ABC'))
+
+    def test_same_first_middle(self):
+        trie = parser.Trie()
+        trie.add_string('ABC')
+        trie.add_string('ABX')
+        self.assertTrue(trie.find_match('ABC'))
+        self.assertTrue(trie.find_match('ABX'))
+
+    def test_different_first(self):
+        trie = parser.Trie()
+        trie.add_string('ABC')
+        trie.add_string('ZBC')
+        self.assertTrue(trie.find_match('ABC'))
+        self.assertTrue(trie.find_match('ZBC'))
+
+    def test_no_match_first(self):
+        trie = parser.Trie()
+        trie.add_string('ABC')
+        self.assertFalse(trie.find_match('?BC'))
+
+    def test_no_match_mid(self):
+        trie = parser.Trie()
+        trie.add_string('ABC')
+        self.assertFalse(trie.find_match('A?C'))
+
+    def test_no_match_last(self):
+        trie = parser.Trie()
+        trie.add_string('ABC')
+        self.assertFalse(trie.find_match('AB?'))
+
+    def test_different_end(self):
+        trie = parser.Trie()
+        trie.add_string('ABC')
+        trie.add_string('ABX')
+        self.assertFalse(trie.find_match('AB?'))
+
 class TestParser(unittest.TestCase):
 
 
@@ -12,7 +64,6 @@ class TestParser(unittest.TestCase):
         '''
         test_list = ['ABC']
         p = parser.Parser(test_list)
-        p.set_wordref(test_list)
         result = p.parse('ABC')
         self.assertEqual(['ABC'], result)
 
@@ -22,6 +73,8 @@ class TestParser(unittest.TestCase):
         Passes when a dictionary with only an 'ABC' entry
         correctly tokenizes 'ABCDE' into ['ABC', 'D', 'E'].
         '''
+        p = parser.Parser(['ABC'])
+        result = p.parse('ABCDE')
     
     
 
