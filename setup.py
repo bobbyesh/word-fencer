59073792cf885b4ed013634d1f21d8217a6007ed
bobbyesh <eshleman@pdx.edu>
2016-07-14 13:51:06 -0700

    Created setup.py.

---
+++ setup.py
@@ -0,0 +1,16 @@
+# setup.py
+
+"""This module simply reads a dictionary reference file, builds a Trie, and then pickles it.
+"""
+
+def build_chinese_simplified():
+    pass
+
+def build_chinese_traditional():
+    pass
+
+def build_cantonese_simplified():
+    pass
+
+def build_cantonese_traditional():
+    pass
